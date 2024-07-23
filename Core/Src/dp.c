9b91d26e57990b9ff932f1e708630361195c9527
pivovarvalera <pivovarvaleri@gmail.com>
2024-07-23 10:21:41 +0300

    Setting via SPI

--- Core/Src/dp.c
+++ Core/Src/dp.c
@@ -1,4 +1,6 @@
 #include <stm32f7xx_hal.h>
+#include <stdio.h>
+#include <stdint.h>
 #define ADC_COUNT 2
 
 struct DpResult{
@@ -6,37 +8,56 @@ struct DpResult{
     int setpoint;
 };
 
-struct DigitalPotentiometer{
-    int csPin;
-};
+void initSPI(){ //что-то с cnf
+    RCC->APB2ENR |= RCC_APB2ENR_SPI1EN; // Включаем тактовый сигнал для SPI1
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; //Включаем тактовый сигнал для GPIOA
 
-void initSPI(){
+    //Сбрасываем конфигурационные биты в нули
+    GPIOA->MODER &= ~(GPIO_MODER_MODER5_Msk | GPIO_MODER_MODER6_Msk | GPIO_MODER_MODER7_Msk);
+   
+    //SCK
+    GPIOA->MODER |= 0x02<<GPIO_MODER_MODER5_Pos; //Максимальная частота
+    GPIOA->OTYPER &= ~GPIO_OTYPER_OT5; //Установка режима pull-push
+  
+    //MISO
+    GPIOA->MODER |= 0x01<<GPIO_MODER_MODER6_Pos;
+    GPIOA->OTYPER &= ~GPIO_OTYPER_OT6;
+  
+    //MOSI
+    GPIOA->MODER |= 0x02<<GPIO_MODER_MODER7_Pos;
+    GPIOA->OTYPER &= ~GPIO_OTYPER_OT7;
 
+
+    //Настройка SPI
+    SPI1->CR1 = (0<<11)     // Размер кадра 8 бит
+    | (0 << SPI_CR1_LSBFIRST_Pos)     // MSB first
+    | (1 << SPI_CR1_SSM_Pos)          // Программное управление SS
+    | (1 << SPI_CR1_SSI_Pos)          // SS в высоком состоянии
+    | (0x03 << SPI_CR1_BR_Pos)        // Скорость передачи: F_PCLK/64
+    | (1 << SPI_CR1_MSTR_Pos);        // Режим "мастер"
+    SPI1->CR1 |= 1<<SPI_CR1_SPE_Pos; //Включаем SPI
 }
 
-void sendCommand(){
-
-}
 int receiveData(){
-
+    //SPI1->DR = 0;
+    while (!(SPI1->SR & SPI_SR_RXNE)); // Ждем, пока буфер приемника не заполнится
+    return SPI1->DR; // Возвращаем прочитанные данные
 }
 
-void closeSPI(){
-
+int sendData(data){
+    while(!(SPI1->SR & SPI_SR_TXE)); //Ждём, пока буфер передатчика не освободится
+    SPI1->DR = data; // Записываем данные для передачи в регистр данных SPI1
 }
 
-//SPI connection
-int communicateWithDp(int csPin){
-    initSPI();
-    sendCommand();
-    int data = receiveData();
-    closeSPI();
-    return data;
+int communicateWithDp(int command){
+    sendData(command); //отправка команды по SPI
+    return receiveData(); // получение ответа от потенциометра
 }
 
-struct DpResult dpReadSetpoint(struct DigitalPotentiometer *dp){
+struct DpResult dpReadSetpoint(){
     struct DpResult dpResult;
-    int err = communicateWithDp(dp->csPin);
+    int command = 0b10000000; //контрольный байт, зависит от потенциометра
+    int err = communicateWithDp(command);
     int setpoint = 0;
     if (err > 0){
         dpResult.err = err;
@@ -48,9 +69,10 @@ struct DpResult dpReadSetpoint(struct DigitalPotentiometer *dp){
     return dpResult;
 }
 
-struct DpResult dpWriteSetpoint(struct DigitalPotentiometer *dp, int newSetpoint){
+struct DpResult dpWriteSetpoint(int newSetpoint){
     struct DpResult dpResult;
-    int err = communicateWithDp(dp->csPin);
+    int command = 0b10000000 | (newSetpoint & 0xFF);
+    int err = communicateWithDp(command);
 
     if (err > 0){
         dpResult.err = err;
@@ -66,17 +88,16 @@ struct DpResult dpWriteSetpoint(struct DigitalPotentiometer *dp, int newSetpoint
 
 int main(void){
 
-    struct  DigitalPotentiometer dp;
-    dp.csPin = 9; //for example
+    initSPI();
 
     struct DpResult adcSetpoint[ADC_COUNT];
 
-    for(int i = 0; i<ADC_COUNT; i++){
-        adcSetpoint[i] = dpReadSetpoint(&dp);
+    for(int i = 0; i < ADC_COUNT; i++){
+        adcSetpoint[i] = dpReadSetpoint();
     }
 
     int newSetpoint = 10;
-    struct DpResult whiteResult = dpWriteSetpoint(&dp, newSetpoint);
+    struct DpResult whiteResult = dpWriteSetpoint(newSetpoint);
 
     if (whiteResult.err > 0) {
         printf("Произошла ошибка при установке новой точки.\n");
